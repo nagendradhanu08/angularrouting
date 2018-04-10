e456fd0212cf8e1534f224239642fedcb6f29de7
nagendradhanu08 <nagendrad502@gmail.com>
2018-04-10 18:16:50 +0530

    Add : Add routing related code.

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path :'home',component: HomeComponent },
+  { path :'employees', component:EmployeesComponent },
+  { path :'', redirectTo: '/home',pathMatch:'full' },
+  { path :'**',component: PageNotFoundComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
